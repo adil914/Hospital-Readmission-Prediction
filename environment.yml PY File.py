ff399ebc686caf4fbf28a7335bdae0c531984d61
Tenshi_encho <adilhossain396@gmail.com>
2026-01-07 02:53:30 -0800

    Update environment.yml PY File.py

--- environment.yml PY File.py
+++ environment.yml PY File.py
@@ -1,14 +1,1 @@
-name: hospital-readmission
-channels:
-  - conda-forge
-  - defaults
-dependencies:
-  - python=3.10
-  - pandas
-  - scikit-learn
-  - numpy
-  - matplotlib
-  - seaborn
-  - jupyter
-  - flake8
-  - pytest
+
