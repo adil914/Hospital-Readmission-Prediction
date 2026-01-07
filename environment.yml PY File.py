10f1c2eb4aa11d599086b1caa11a4c87bf2f8ea1
adil914 <adilhossain396@gmail.com>
2026-01-07 02:18:54 -0800

    Add files via upload

---
+++ environment.yml PY File.py
@@ -0,0 +1,14 @@
+name: hospital-readmission
+channels:
+  - conda-forge
+  - defaults
+dependencies:
+  - python=3.10
+  - pandas
+  - scikit-learn
+  - numpy
+  - matplotlib
+  - seaborn
+  - jupyter
+  - flake8
+  - pytest
